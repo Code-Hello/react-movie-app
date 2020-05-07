e16a15f044df0916e7450bb58c52265f8e8ff1c4
Code-Hello <m.tuto@protonmail.com>
2020-05-07 19:08:52 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import FormMovie from "./components/FormMovie";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <FormMovie />
+    </div>
+  );
 }
 
 export default App;
